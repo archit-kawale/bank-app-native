a4be8b13713b95d401b778db3a2c64bdc20c52f1
Sidhanth Bandodkar <sidhanth@synergyconnect.in>
2024-08-19 10:39:45 +0530

    Push

---
+++ app/(tabs)/Map.jsx
@@ -0,0 +1,11 @@
+import {Text,SafeAreaView} from 'react-native'
+
+const Map = () => {
+    return (
+        <SafeAreaView>
+            <Text style={{color:'#fff'}}>Map</Text>
+        </SafeAreaView>
+    )
+};
+
+export default Map;
